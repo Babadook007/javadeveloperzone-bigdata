de64124b16fcf3607fe0fc39502cb446d988296e
bigdataspots <prashant.n.khunt@gmail.com>
2019-02-21 23:49:01 +0530

    DB Access using Hadoop

--- HadoopDBInputWritable/src/main/java/com/jdz/DBDriver.java
+++ HadoopDBInputWritable/src/main/java/com/jdz/DBDriver.java
@@ -14,60 +14,51 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class DBDriver extends Configured implements Tool{
+public class DBDriver extends Configured implements Tool {
 
 	public int run(String[] args) throws Exception {
-		
+
 		Configuration conf = new Configuration();
-		
-	     DBConfiguration.configureDB(conf,
-	     "com.mysql.jdbc.Driver",   // driver class
-	     "jdbc:mysql://192.168.0.23:3306/retail", // db url
-	     "root",    // user name
-	     "root"); //password
 
-	     Job job = Job.getInstance(conf);
-	     job.setJarByClass(DBDriver.class);
-	     job.setMapperClass(DBMapper.class);
-	     
-	  //   job.setReducerClass(DBReducer.class);
-	     job.setMapOutputKeyClass(Text.class);
-	     job.setMapOutputValueClass(IntWritable.class);
-	    
-	     job.setOutputKeyClass(TextOutputFormat.class);
-	     job.setOutputValueClass(NullWritable.class);
-	    
-	     job.setInputFormatClass(DBInputFormat.class);
-	     job.setOutputFormatClass(TextOutputFormat.class);
+		DBConfiguration.configureDB(conf, "com.mysql.jdbc.Driver", // driver class
+				"jdbc:mysql://localhost:3306/retail", // db url
+				"root", // user name
+				"root"); // password
 
-	     DBInputFormat.setInput(
-	     job,
-	     DBInputWritable.class,
-	     "users",   //input table name
-	     null,
-	     null,
-	     new String[] { "user_id", "user_name", "department" }  // table columns
-	     );
-	     
-	     FileOutputFormat.setOutputPath(job, new Path(args[0]));
-	     
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(DBDriver.class);
+		job.setMapperClass(DBMapper.class);
 
-	     System.exit(job.waitForCompletion(true) ? 0 : 1);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(NullWritable.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(NullWritable.class);
+
+		job.setInputFormatClass(DBInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+
+		DBInputFormat.setInput(job, DBInputWritable.class, "users", // input table name
+				null, null, new String[] { "user_id", "user_name", "department" } // table columns
+		);
+
+		FileOutputFormat.setOutputPath(job, new Path(args[0]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
 		return 0;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		try{
-		
-		int result = ToolRunner.run(new Configuration(), new DBDriver(), args);
-		
-		System.out.println("job status ::"+result);
-		}
-		catch(Exception exception)
-		{
+
+		try {
+
+			int result = ToolRunner.run(new Configuration(), new DBDriver(), args);
+
+			System.out.println("job status ::" + result);
+			
+		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
-			
+
 	}
 }
